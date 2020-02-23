3540917df8ab6f3dc237ebb5dff14057db386505
YaroslavR <666anastyalena@mail.ru>
2020-02-23 16:25:30 +0300

    feat:task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }
+  var MinNumber = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(MinNumber > array[i]){
+      MinNumber = array[i];
+    }
+  }
+  return MinNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }
+  return array.reduce((acc, cur) => {if(acc > cur){return acc} else {return acc = cur}});
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  }
+   var sum = array.reduce((acc, cur) => acc + cur, 0);
+   return sum / array.length;
 }
